13590823062e9e8864d3c46dfee6bfdeb7fa7f0e
Eric Pezzulo <eric.pezzulo@gmail.com>
2023-10-13 20:50:00 -0400

    added mobile menu

---
+++ src/components/Menu/MobileMenu.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { fly } from 'svelte/transition';
+	export let closeMenu: () => void;
+</script>
+
+<div class="relative w-full md:hidden">
+	<ul
+		in:fly={{ y: -100, duration: 300, opacity: 0.35 }}
+		out:fly={{ y: -150, duration: 400, opacity: 0.35 }}
+		class="absolute p-3 bg-white flex flex-col w-full border-b border-slate-200 z-10"
+	>
+		<a href="/" on:click={closeMenu}>
+			<li class="py-2 pl-4 rounded text-lg font-medium">Home</li>
+		</a>
+
+		<hr class="border-1 border-slate-200" />
+		<a href="/mealplans" on:click={closeMenu}>
+			<li class="py-2 pl-4 rounded text-lg font-medium">Meal Plans</li>
+		</a>
+		<hr class="border-1 border-slate-200" />
+		<a href="/workouts" on:click={closeMenu}>
+			<li class="py-2 pl-4 rounded text-lg font-medium">Workouts</li>
+		</a>
+	</ul>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,7 @@
 	<title>FitTrack :D</title>
 </svelte:head>
 
-<div>
-	<p>//todo:::</p>
-	<p>Upcoming Workouts</p>
-	<p>Upcoming Meals</p>
-	<p>Analytics</p>
-</div>
+<p>//todo:::</p>
+<p>Upcoming Workouts</p>
+<p>Upcoming Meals</p>
+<p>Analytics</p>
